eb912beb9177ababdedc78eecd96eb11b88272e4
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-04-26 10:19:30 +0530

    add logger middleware & middleware chain in main.go

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -1,9 +1,17 @@
 package middleware
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
-func Logger() http.Handler {
+func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		
+		start := time.Now()
+		reqID := r.Context().Value(RequestIDkey)
+
+		next.ServeHTTP(w, r)
+		log.Printf("requestID=%s,path=%s, methode=%s,duration=%s", reqID, r.URL.Path, r.Method, time.Since(start))
 	})
 }
