0cf239c54aa97b2fd31a4cbe2f39138848555ac6
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-05-25 15:05:35 +0530

    add role in user model with modular way

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -2,15 +2,17 @@ package model
 
 import "time"
 
+type Role string
+
 const (
-	RoleUser  = "user"
-	RoleAdmin = "admin"
+	RoleUser  Role = "user"
+	RoleAdmin Role = "admin"
 )
 
 type User struct {
 	ID           int       `json:"id"`
 	Email        string    `json:"email"`
 	PasswordHash string    `json:"-"` // json:"-" prevents password hash from leaking in API responses.
-	Role         string    `json:"role"`
+	Role         Role      `json:"role"`
 	CreatedAt    time.Time `json:"created_at"`
 }
