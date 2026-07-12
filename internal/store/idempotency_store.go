3c51ee7fe6a633f2d49318bfcea853771326b524
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-07-12 21:49:14 +0530

    add userid & expiration in idempotency db operations

--- internal/store/idempotency_store.go
+++ internal/store/idempotency_store.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"time"
 )
 
 type IdempotencyStore struct {
@@ -17,20 +18,39 @@ func NewIdempotencyStore(db *sql.DB) *IdempotencyStore {
 	}
 }
 
-func (s *IdempotencyStore) GetIdempotency(ctx context.Context, idempotencyKey string) (*model.Idempotency, error) {
-	query := `SELECT * FROM idempotency_keys WHERE idempotency_key = $1`
+func (s *IdempotencyStore) GetIdempotency(ctx context.Context, userID int, idempotencyKey string) (*model.Idempotency, error) {
+	// Idempotency keys are unique per user, not globally.
+	// Using both user_id and idempotency_key ensures we retrieve the correct
+	// cached response and prevents cross-user collisions.
+	query := `SELECT 
+	 id,
+	 idempotency_key,
+	 user_id,
+	 status_code,
+	 response,
+	 created_at,
+	 expires_at
+	 FROM idempotency_keys WHERE user_id = $1 AND idempotency_key = $2`
 	var idempotency model.Idempotency
-	err := s.db.QueryRowContext(ctx, query, idempotencyKey).Scan(&idempotency.UserID, &idempotency.IdempotencyKey, &idempotency.Response)
+	err := s.db.QueryRowContext(ctx, query, userID, idempotencyKey).Scan(
+		&idempotency.ID,
+		&idempotency.IdempotencyKey,
+		&idempotency.UserID,
+		&idempotency.StatusCode,
+		&idempotency.Response,
+		&idempotency.CreatedAt,
+		&idempotency.ExpiresAt,
+	)
 	if err != nil {
 		return nil, err
 	}
 	return &idempotency, nil
 }
 
-func (s *IdempotencyStore) InsertIdempotency(ctx context.Context, userID int, idempotencyKey string, response json.RawMessage) error {
-	query := `INSERT INTO idempotency_keys(idempotency_key,user_id,response) VALUES ($1,$2,$3)`
+func (s *IdempotencyStore) InsertIdempotency(ctx context.Context, userID int, idempotencyKey string, statusCode int, response json.RawMessage, expiresAt time.Time) error {
+	query := `INSERT INTO idempotency_keys(idempotency_key,user_id,status_code,response, expires_at) VALUES ($1,$2,$3,$4,$5)`
 
-	_, err := s.db.ExecContext(ctx, query, userID, response, idempotencyKey)
+	_, err := s.db.ExecContext(ctx, query, idempotencyKey, userID, statusCode, response, expiresAt)
 	if err != nil {
 		return err
 	}
